e616e883b4c9edbcb14f816fee6bc1d46477c943
Scarlett <kscarletthunt@gmail.com>
2023-09-29 20:20:10 -0600

    updating fortunes and fixing route issue

--- src/routes/BotRoutes.ts
+++ src/routes/BotRoutes.ts
@@ -1,10 +1,12 @@
-import express from 'express'
-import getCrystalBallFortune from '../controllers/BotController.js'
+import express from 'express';
+import getCrystalBallFortune from '../controllers/BotController.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/crystalBall').get(getCrystalBallFortune)
+router.route('/crystalBall').get((req, res) => {
+  res.send(getCrystalBallFortune());
+});
 
-router
+router;
 
-export default router
+export default router;
